5afaf598176b2ad98a634809d1e0416e74b6e2fc
Alex Swan <alex.swan1991@gmail.com>
2021-03-27 22:07:51 +0000

    Added an event lister to js file that when a grid square is hovered over it will turn black

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,5 +1,5 @@
 const container = document.querySelector(".container");
-
+const gridSquare = document.getElementsByClassName("gridSquare");
 // This function creates 16 rows with 16 squares in each row
 let fullGrid = function () {
   for (let i = 0; i < 16; i++) {
@@ -9,9 +9,15 @@ let fullGrid = function () {
     for (let i = 0; i < 16; i++) {
       square = document.createElement("DIV");
       row.appendChild(square);
-      square.className = "square";
+      square.className = "gridSquare";
     }
   }
 };
-
 fullGrid();
+
+// This event listener makes it so when user hovers over a grid square it turns changes color and turns black.
+for (let i = 0; i < gridSquare.length; i++) {
+  gridSquare[i].addEventListener("mouseover", () => {
+    gridSquare[i].style.backgroundColor = "black";
+  });
+}
